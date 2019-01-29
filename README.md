d60d6b6382d9cb8012355aaf5be7dfc9b0a52bee
pavroo <pavroo@onet.eu>
2019-01-29 18:34:03 +0100

    initial

---
+++ README.md
@@ -0,0 +1,48 @@
+Sparky Tube
+Download a video file from a web site and convert it to MP3 and MP4 output file.
+
+Copyright (C) 2019 Elton Fabricio Ferreira & Paweł Pijanowski
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+Dependencies:
+-------------
+apt
+coreutils
+curl
+ffmpeg
+grep
+sparky-xterm
+sudo
+xdg-utils
+yad
+youtube-dl
+
+Conflicts:
+-------------
+sparkytube
+
+Replaces:
+-------------
+sparkytube
+
+Install:
+-------------
+su (or sudo) 
+./install.sh
+
+Uninstall:
+-------------
+su (or sudo)
+./install.sh uninstall
---
+++ install.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+#
+#  This program is free software; you can redistribute it and/or
+#  modify it under the terms of the GNU General Public License as
+#  published by the Free Software Foundation; either version 2 of the
+#  License, or (at your option) any later version.
+#
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with this program; if not, write to the Free Software Foundation,
+#  Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301, USA
+
+if [ "$1" = "uninstall" ]; then
+	rm -f /usr/bin/sparky-quick-mp3
+	rm -f /usr/bin/sparky-quick-mp4
+	rm -f /usr/share/applications/sparky-tube-mp3.desktop
+	rm -f /usr/share/applications/sparky-tube-mp4.desktop
+	rm -rf /usr/share/sparky/sparky-tube
+else
+	cp bin/* /usr/bin/
+	cp share/* /usr/share/applications/
+	if [ ! -d /usr/share/sparky/sparky-tube ]; then
+		mkdir -p /usr/share/sparky/sparky-tube
+	fi
+	cp lang/* /usr/share/sparky/sparky-tube/
+fi
